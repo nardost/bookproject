d3141dd1d7070b2dfe98b662917ca8b646cc8504
Nardos Tessema <nardos.tessema@gmail.com>
2017-10-08 17:59:56 -0500

    testing github webhooks

--- src/test/java/bookprojectTest/TestBook.java
+++ src/test/java/bookprojectTest/TestBook.java
@@ -13,6 +13,6 @@ public class TestBook {
 	Book book = new Book("And Quiet Flows the Don", 1000, "String language", 30, Date.valueOf("1990-01-01"), "String isbn", "String author");
 	@Test
 	public void performFirstTest() {
-		assertEquals("Wrong Title", book.getTitle());
+		assertEquals("And Quiet Flows the Don", book.getTitle());
 	}
 }
